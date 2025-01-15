a79ced2af86b11ca5e373b5307b496e643edb209
ykjung <ykjung@scgs.co.kr>
2025-01-15 09:02:15 +0900

    Refactor useDayStore to remove persistence middleware and simplify state management

--- store/useDayStore.ts
+++ store/useDayStore.ts
@@ -1,24 +1,19 @@
 import dayjs from 'dayjs';
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 
 export const useDayStore = create<{
   day: string | null;
   setDay: (day: string) => void;
 }>()(
   devtools(
-    persist(
-      (set) => ({
-        day: dayjs().format('YYYY-MM-DD'),
-        setDay: (day: string) =>
-          set({
-            day,
-          }),
-      }),
-      {
-        name: 'day-storage', // unique name for the storage
-      }
-    ),
+    (set) => ({
+      day: dayjs().format('YYYY-MM-DD'),
+      setDay: (day: string) =>
+        set({
+          day,
+        }),
+    }),
     {
       name: 'day',
     }
