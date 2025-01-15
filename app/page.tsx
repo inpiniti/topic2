2b3def57e2f74bb10569c938719251d9fe69256a
inpiniti <younginpiniti@gmail.com>
2025-01-16 01:08:25 +0900

    Refactor code style for consistency and readability in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { useTopicStore } from '@/store/useTopicStore';
+import { useTopicStore } from "@/store/useTopicStore";
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
-} from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { ReactNode, useState } from 'react';
-import dayjs from 'dayjs';
-import { useDayStore } from '@/store/useDayStore';
+} from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { ReactNode, useState } from "react";
+import dayjs from "dayjs";
+import { useDayStore } from "@/store/useDayStore";
 
 const queryClient = new QueryClient();
 
@@ -33,57 +33,57 @@ const Wrapper = ({ children }: { children: ReactNode }) => {
 };
 
 const DateChange = () => {
-  const daysOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
+  const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
   const today = dayjs();
-  const [startOfWeek, setStartOfWeek] = useState(today.startOf('week'));
+  const [startOfWeek, setStartOfWeek] = useState(today.startOf("week"));
   const { day, setDay } = useDayStore();
 
   const handleDayClick = (date: string) => {
-    if (dayjs(date).isAfter(today, 'day'))
-      return alert('미래의 날짜는 선택 불가능합니다.');
+    if (dayjs(date).isAfter(today, "day"))
+      return alert("미래의 날짜는 선택 불가능합니다.");
     setDay(date);
   };
 
   const handlePrev = () => {
-    const prevWeek = startOfWeek.subtract(1, 'week');
+    const prevWeek = startOfWeek.subtract(1, "week");
     setStartOfWeek(prevWeek);
-    setDay(prevWeek.add(today.day(), 'day').format('YYYY-MM-DD'));
+    setDay(prevWeek.endOf("week").format("YYYY-MM-DD"));
   };
 
   const handleNext = () => {
-    if (startOfWeek.add(1, 'week').isAfter(today, 'day'))
-      return alert('미래의 날짜는 선택 불가능합니다.');
-    const nextWeek = startOfWeek.add(1, 'week');
+    if (startOfWeek.add(1, "week").isAfter(today, "day"))
+      return alert("미래의 날짜는 선택 불가능합니다.");
+    const nextWeek = startOfWeek.add(1, "week");
     setStartOfWeek(nextWeek);
-    setDay(nextWeek.add(today.day(), 'day').format('YYYY-MM-DD'));
+    setDay(nextWeek.startOf("week").format("YYYY-MM-DD"));
   };
 
   return (
     <div className="shrink-0 px-4 pt-2 pb-1 flex flex-col text-white items-center">
-      <div>{dayjs(day).format('YYYY.MM')}</div>
+      <div>{dayjs(day).format("YYYY.MM")}</div>
       <div className="flex justify-between items-center w-full">
         <span
           className="text-white cursor-pointer px-2 py-1 hover:bg-zinc-800 rounded-xl"
           onClick={handlePrev}
         >
-          {'〈'}
+          {"〈"}
         </span>
         {daysOfWeek.map((_day, index) => {
-          const date = startOfWeek.add(index, 'day').format('DD');
-          const fullDate = startOfWeek.add(index, 'day').format('YYYY-MM-DD');
+          const date = startOfWeek.add(index, "day").format("DD");
+          const fullDate = startOfWeek.add(index, "day").format("YYYY-MM-DD");
           return (
             <div
               key={index}
               className={`text-white cursor-pointer px-2 py-1 rounded-xl ${
                 day === fullDate
-                  ? 'bg-blue-400 font-bold hover:bg-blue-500'
-                  : 'hover:bg-zinc-800'
+                  ? "bg-blue-400 font-bold hover:bg-blue-500"
+                  : "hover:bg-zinc-800"
               }`}
               onClick={() => handleDayClick(fullDate)}
             >
               <div
                 className={`font-thin ${
-                  day === fullDate ? 'text-white' : 'text-zinc-400'
+                  day === fullDate ? "text-white" : "text-zinc-400"
                 }`}
               >
                 {date}
@@ -96,7 +96,7 @@ const DateChange = () => {
           className="text-white cursor-pointer px-2 py-1 hover:bg-zinc-800 rounded-xl"
           onClick={handleNext}
         >
-          {'〉'}
+          {"〉"}
         </span>
       </div>
     </div>
@@ -108,12 +108,12 @@ const List = () => {
   const { topic, setTopic } = useTopicStore();
   const { day } = useDayStore();
 
-  const payload = dayjs(day).isSame(dayjs(), 'day')
-    ? 'realtime'
+  const payload = dayjs(day).isSame(dayjs(), "day")
+    ? "realtime"
     : `daily&date=${day}`;
 
   const { isPending, error, data } = useQuery({
-    queryKey: ['repoData', day],
+    queryKey: ["repoData", day],
     queryFn: () =>
       fetch(`/api/google/topics?range=${payload}`).then((res) => res.json()),
     staleTime: 3600000, // 1 hour in milliseconds
@@ -121,12 +121,12 @@ const List = () => {
 
   const handleItemClick = (topic: string) => {
     setTopic(topic);
-    router.push('/topic');
+    router.push("/topic");
   };
 
-  if (isPending) return 'Loading...';
+  if (isPending) return "Loading...";
 
-  if (error) return 'An error has occurred: ' + error.message;
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <div className="grow overflow-hidden h-full p-2 overflow-y-scroll no-scrollbar">
@@ -138,8 +138,8 @@ const List = () => {
                 className={`p-2 hover:text-blue-400 hover:bg-zinc-800 cursor-pointer
                   ${
                     topic === item.topic
-                      ? 'text-blue-300 bg-zinc-800 font-bold'
-                      : 'text-white'
+                      ? "text-blue-300 bg-zinc-800 font-bold"
+                      : "text-white"
                   }`}
                 onClick={() => handleItemClick(item.topic)}
               >
